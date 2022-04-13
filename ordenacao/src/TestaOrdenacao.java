213fd770ce7cc0bdf721f713cb3cc6635d28f36a
danilolopesabreu <danilo.bcc@gmail.com>
2022-04-12 23:41:10 -0300

    insertion sort

--- ordenacao/src/TestaOrdenacao.java
+++ ordenacao/src/TestaOrdenacao.java
@@ -10,21 +10,42 @@ public class TestaOrdenacao {
             new Produto("Fusca", 17000)
         };
 
-        for (int atual = 0; atual < produtos.length; atual++) {
-
-            int menor = buscaMenor(produtos, atual, produtos.length - 1);
-
-            Produto produtoAtual = produtos[atual];
-            Produto produtoMenor = produtos[menor];
-
-            produtos[atual] = produtoMenor;
-            produtos[menor] = produtoAtual;
-        }
+//        selectionSort(produtos, produtos.length);
+        insertionSort(produtos, produtos.length);
 
         for (Produto produto : produtos) {
             System.out.println(produto.getNome() + " custa " + produto.getPreco());
         }
     }
+
+	private static void troca(Produto[] produtos, int atual, int menor) {
+		Produto produtoAtual = produtos[atual];
+		Produto produtoMenor = produtos[menor];
+
+		produtos[atual] = produtoMenor;
+		produtos[menor] = produtoAtual;
+	}
+	
+	private static void selectionSort(Produto[] produtos, int quantidadeDeElementos) {
+	 for(int atual = 0; atual < quantidadeDeElementos -1; atual++) {
+	     System.out.println("Estou na casinha " + atual);
+
+	     int menor = buscaMenor(produtos, atual, quantidadeDeElementos -1);
+	     troca(produtos, atual, menor);
+	 }
+	}
+    
+    private static void insertionSort(Produto[] produtos, int quantidadeDeElementos) {
+    	 for (int atual = 1; atual < quantidadeDeElementos; atual++) {
+    	     System.out.println("Estou na casinha " + atual);
+
+    	     int analise = atual;
+    	     while(analise > 0 && produtos[analise].getPreco() < produtos[analise -1].getPreco() ){
+    	         troca(produtos, analise, analise -1);
+    	         analise--;
+    	     }
+    	 }
+    	}
 
     private static int buscaMenor(Produto[] produtos, int inicio, int termino) {
 
