90bcbe8b080b29dd1cb6e510af35d9a856962bde
danilolopesabreu <danilo.bcc@gmail.com>
2022-04-16 15:33:24 -0300

    ajustes merge sort

--- ordenacao/src/merge_sort/MergeSort.java
+++ ordenacao/src/merge_sort/MergeSort.java
@@ -1,13 +1,11 @@
 package merge_sort;
 
-import java.util.Iterator;
-
-public class TestaOrdenacao_v3 {
+public class MergeSort {
 	public static void main(String[] args) {
 		
 		final Aluno[] alunos4 = { new Aluno("A", 8), new Aluno("B", 5) };
 		
-		Aluno[] outrasAlunos = {
+		Aluno[] outrosAlunos = {
 		        new Aluno("Jonas", 2),
 		        new Aluno("Fernando", 2.3),
 		        new Aluno("Ana", 7.1),
@@ -20,24 +18,52 @@ public class TestaOrdenacao_v3 {
 		};
 		
 		Aluno[] notas = {
+				new Aluno("Enzo", 6.6),
 		        new Aluno("Jonas", 4),
 		        new Aluno("Fernando", 5),
 		        new Aluno("Ana", 8.5),
+		        new Aluno("Enzo", 9.1),
 		        new Aluno("Guilherme", 9),
 		        new Aluno("Alberto", 3),
+		        new Aluno("Enzo", 8.3),
 		        new Aluno("Maria", 6.7),
 		        new Aluno("Paulo", 7),
 		        new Aluno("Enzo", 9.3),
 		        new Aluno("Lucia", 10)
 		};
 		
-		intercala(notas, 0, 4, notas.length);
+		Aluno[] arrayCopia = notas;  
+		
+		ordena(arrayCopia, 0, arrayCopia.length);
+		
+//		intercala(arrayCopia, 0, 1, 2);
+//		intercala(arrayCopia, 2, 3, 4);
+//		intercala(arrayCopia, 0, 2, 4);
+//
+//		intercala(arrayCopia, 4, 5, 6);
+//		intercala(arrayCopia, 6, 7, 8);
+//		intercala(arrayCopia, 4, 6, 8);
+//
+//		intercala(arrayCopia, 0, 4, 8);
+//
+//		intercala(arrayCopia, 0, 8, 9);
 		 		
-		for (int i = 0; i < notas.length; i++) {
-			System.out.println(notas[i]);
+		for (int i = 0; i < arrayCopia.length; i++) {
+			System.out.println(arrayCopia[i]);
 		}
 		
 	}
+	
+	private static void ordena(Aluno[] notas, int inicio, int termino) {
+
+		  int quantidade = termino - inicio;
+		  if(quantidade > 1) {
+		    int meio = (inicio + termino) / 2;
+		    ordena(notas, inicio, meio);
+		    ordena(notas, meio, termino);
+		    intercala(notas, inicio, meio, termino);
+		  }
+		}
 	
 	private static void intercala(Aluno[] array, int inicio , int meio, int fim) {
 		
@@ -60,6 +86,7 @@ public class TestaOrdenacao_v3 {
 				arrayOrdenado[atual] = aluno2;
 				atualMeio++;
 			}
+			
 			atual++;
 		}
 		
@@ -75,6 +102,7 @@ public class TestaOrdenacao_v3 {
 			atual++;
 		}
 		
+		//sobreescreve os valores originais com os valores ordenados.
 		for (int i = 0; i < atual; i++) {
 			array[inicio + i] = arrayOrdenado[i];
 		}
